e872d7bcd88a4a5384cdcff9d5281821dde2a451
MD Tahfiz Chowdhury <tahfiz9495@gmail.com>
2024-09-08 01:15:14 +0600

    Declared and print the name of creator

--- App.java
+++ App.java
@@ -1,6 +1,8 @@
 public class App {
     public static void main(String[] args) throws Exception 
     {
-        
+        System.out.println("Printer App");
+        String creator = "Tahfiz.";
+        System.out.println("This app is made by: " + creator);
     }
 }
