e57d639e9cf09b928f38a969117884c933b0329f
MD Tahfiz Chowdhury <tahfiz9495@gmail.com>
2024-09-08 01:38:10 +0600

    Formatted the code

--- App.java
+++ App.java
@@ -1,24 +1,32 @@
 public class App {
     public static void main(String[] args) throws Exception 
     {
-        System.out.println("Printer App");
         String creator = "Tahfiz.";
-        System.out.println("This app is made by: " + creator);
         int Number1 = 20;
         int Number2 = 10;
-        System.out.println("The value of Number1 is " + Number1);
-        System.out.println("The value of Number2 is " + Number2);
+
         int Multiplication;
         Multiplication = Number1 * Number2;
-        System.out.println(Number1 + " * " + Number2 + " = " + Multiplication);
+
         int addition;
         addition = Number1 + Number2;
-        System.out.println(Number1 + " + " + Number2 + " = " + addition);
+
         int division;
         division = Number1 / Number2;
-        System.out.println(Number1 + " / " + Number2 + " = " + division);
+
         int substraction;
         substraction = Number1 - Number2;
+
+        System.out.println("Printer App");
+
+        System.out.println("This app is made by: " + creator);
+
+        System.out.println("The value of Number1 is " + Number1);
+        System.out.println("The value of Number2 is " + Number2);
+
+        System.out.println(Number1 + " * " + Number2 + " = " + Multiplication);
+        System.out.println(Number1 + " + " + Number2 + " = " + addition);
+        System.out.println(Number1 + " / " + Number2 + " = " + division);
         System.out.println(Number1 + " - " + Number2 + " = " + substraction);
     }
 }
